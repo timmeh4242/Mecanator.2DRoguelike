61a42bdd2c61f5db74a7e63579258212bc3ced26
Tim Riley <tim@finergames.com>
2017-09-07 09:18:29 +0800

    - imported mecanator - started work on new inspector

---
+++ Assets/Mecanator/Actions/SetAnimatorParameter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SetAnimatorParameter : StateMachineAction
+{
+	public string ParameterName;
+	public int Value;
+
+//	private int ParameterHash;
+
+//	void Awake()
+//	{
+//		ParameterHash = Animator.StringToHash (ParameterName);
+//	}
+
+	public override void Execute (StateMachineActionObject smao)
+	{
+		smao.Animator.SetInteger (ParameterName, Value);
+	}
+}
